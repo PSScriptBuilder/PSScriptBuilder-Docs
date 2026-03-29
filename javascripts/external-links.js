690d08164e94420a8a882b6965473d6d08a8783f
PSScriptBuilder <PSScriptBuilder@users.noreply.github.com>
2026-03-29 04:50:04 +0000

    deploy: PSScriptBuilder/PSScriptBuilder@e7cd85290772e4200d5388d68c1924d600ef0e9a

--- javascripts/external-links.js
+++ javascripts/external-links.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     var links = document.querySelectorAll('a[href]');
     links.forEach(function (link) {
         var href = link.getAttribute('href');
-        if (href && (href.startsWith('http://') || href.startsWith('https://'))
-            && link.hostname !== window.location.hostname) {
-            link.setAttribute('target', '_blank');
-            link.setAttribute('rel', 'noopener noreferrer');
+        if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
+            try {
+                var url = new URL(href);
+                if (url.hostname !== window.location.hostname) {
+                    link.setAttribute('target', '_blank');
+                    link.setAttribute('rel', 'noopener noreferrer');
+                }
+            } catch (e) {}
         }
     });
 });
