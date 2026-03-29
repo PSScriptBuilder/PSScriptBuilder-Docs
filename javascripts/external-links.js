d15463a5f24c226586e030c756dc125a163a4639
PSScriptBuilder <PSScriptBuilder@users.noreply.github.com>
2026-03-29 04:56:56 +0000

    deploy: PSScriptBuilder/PSScriptBuilder@6ebaf9d1a042650d1d6bd261dcf3b645f0191663

--- javascripts/external-links.js
+++ javascripts/external-links.js
@@ -1,15 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.querySelectorAll('a[href]');
-    links.forEach(function (link) {
-        var href = link.getAttribute('href');
-        if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
-            try {
-                var url = new URL(href);
-                if (url.hostname !== window.location.hostname) {
-                    link.setAttribute('target', '_blank');
-                    link.setAttribute('rel', 'noopener noreferrer');
-                }
-            } catch (e) {}
-        }
-    });
-});
+(function () {
+    function processLinks() {
+        var links = document.querySelectorAll('a[href]');
+        links.forEach(function (link) {
+            var href = link.getAttribute('href');
+            if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
+                try {
+                    var url = new URL(href);
+                    if (url.hostname !== window.location.hostname) {
+                        link.setAttribute('target', '_blank');
+                        link.setAttribute('rel', 'noopener noreferrer');
+                    }
+                } catch (e) {}
+            }
+        });
+    }
+
+    if (typeof document$ !== 'undefined') {
+        document$.subscribe(processLinks);
+    } else {
+        document.addEventListener('DOMContentLoaded', processLinks);
+    }
+}());
